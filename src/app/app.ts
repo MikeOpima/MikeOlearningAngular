9b30972d273d0112b8df405ed5c2e821a66990da
Mike OPima <MO03@myscc.ca>
2026-01-20 15:36:45 -0500

    create an array in app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 // @ts-ignore
 import { RouterOutlet } from "@angular/router";
+import {MyData} from './Interfaces/fund-project';
 
 // @ts-ignore
 @Component({
@@ -14,5 +15,60 @@ export class App {
   GOD: string = 'YHWH';
   currentYear: number = 2026;
   isLoggedIn: boolean = true;
+
+  // Initialize the array with 6 funded projects
+  fundedProjects: MyData[] = [
+    {
+      fundId: 1, fundCode: 'MG256', fundName: 'Jackson Park', fundManager: 'MG Asset Management Inc',
+      portfolioManager: "MG Asset Management Inc",
+      signupDate: "December 17, 2022",
+      startPrice: 650
+    },
+    {
+      fundId: 2,
+      fundCode: "RB234",
+      fundName: "Marco Polo",
+      fundManager: "RBG Asset Management Inc",
+      portfolioManager: "RBG Investments",
+      signupDate: "January 17, 2008",
+      startPrice: 1200
+    },
+    {
+      fundId: 3,
+      fundCode: "MG700",
+      fundName: "Jacob Mitchell",
+      fundManager: "MG Asset Management Inc",
+      portfolioManager: "MG Asset Management Inc",
+      signupDate: "March 08, 2011",
+      startPrice: 25000
+    },
+    {
+      fundId: 4,
+      fundCode: "BM461",
+      fundName: "Milly Morrison",
+      fundManager: "BM Asset Management Inc",
+      portfolioManager: "BM Asset Management Inc",
+      signupDate: "May 08, 2011",
+      startPrice: 2500
+    },
+    {
+      fundId: 5,
+      fundCode: "RB700",
+      fundName: "Jacob Mitchell",
+      fundManager: "RBG Asset Management Inc",
+      portfolioManager: "RBG Investments",
+      signupDate: "November 30, 2020",
+      startPrice: 30430
+    },
+    {
+      fundId: 6,
+      fundCode: "ZB700",
+      fundName: "Michelle Kalps",
+      fundManager: "Zash Brothers Inc",
+      portfolioManager: "Zash Brothers Management",
+      signupDate: "November 30, 2021",
+      startPrice: 3100
+    }
+    ];
 
 }
