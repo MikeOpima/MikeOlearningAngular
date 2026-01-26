49b2154b0de951e7b581bcc3b9c5c8ecb66dc8cd
Mike OPima <MO03@myscc.ca>
2026-01-26 12:37:12 -0500

    refactor the  ContentList and ContentListItem

---
+++ src/app/Component/fundProject-list-item/content-list-item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fundProject-list-item',
+  imports: [],
+  templateUrl: './content-list-item.html',
+  styleUrl: './content-list-item.css',
+})
+export class ContentListItem {
+
+}
---
+++ src/app/Component/fundProject-list/content-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fundProject-list',
+  imports: [],
+  templateUrl: './content-list.html',
+  styleUrl: './content-list.css',
+})
+export class ContentList {
+
+}
