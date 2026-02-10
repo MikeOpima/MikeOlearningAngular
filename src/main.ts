61a463d084770867076970d28a92339ba54f839b
Mike OPima <MO03@myscc.ca>
2026-02-09 22:16:44 -0500

    ProvideRouter * listing routes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import {provideRouter, Routes} from '@angular/router';
+import {FundProjectList} from './app/Component/fundProject-list/fundProject-list';
+import {FundProjectListItem} from './app/Component/fundProject-list-item/fundProject-list-item';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes = [
+  {path: '', redirectTo: '/funds', pathMatch: 'full'},
+  {path: 'funds', component: FundProjectList},
+  {path: 'funds', component: FundProjectListItem},
+  // {path: 'modify-funds', component: ModifyFundProjectList},
+];
+bootstrapApplication(App, {
+  providers: [provideRouter(routes)]
+}).then((err) => console.error(err));
+
