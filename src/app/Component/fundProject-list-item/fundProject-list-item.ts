5f48b1a4e7fd0484a3cc4f3de6d31d6ae82d1c36
Mike Opima <MO03@myscc.ca>
2026-03-05 16:34:44 -0500

    Steps 8-9 *Updating listComponente and listItemComponent to display data by fetching from the service *Display error messages in UI for failed operations

--- src/app/Component/fundProject-list-item/fundProject-list-item.ts
+++ src/app/Component/fundProject-list-item/fundProject-list-item.ts
@@ -1,7 +1,8 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FundData} from '../../Interfaces/fund-data';
 import {NgOptimizedImage} from '@angular/common';
-import {RouterLink, RouterLinkActive} from '@angular/router';
+import {ActivatedRoute, Router, RouterLink, RouterLinkActive} from '@angular/router';
+import {FundService} from '../../Services/fund-service';
 
 @Component({
   selector: 'app-fundProject-list-item',
@@ -13,7 +14,61 @@ import {RouterLink, RouterLinkActive} from '@angular/router';
   templateUrl: './fundProject-list-item.html',
   styleUrl: './fundProject-list-item.css',
 })
-export class FundProjectListItem {
-  @Input() fundedProjects?: FundData;
+export class FundProjectListItem implements OnInit {
+  // Needs to be  |   undef because there won't always be a fund that's clicked on
+  fund: FundData | undefined;  // fund to be displayed
+  fundList: FundData[] = [];  // to store the list of students
+  currentIndex: number = 0;   // to track the current index
+  error: string | null = null;  // to store any errors
+  constructor(
+    private route: ActivatedRoute,
+    private fundService: FundService,
+    private router: Router
+  ) { }
+  //rewrote onInit to get the list of funds and the current fund
+  ngOnInit(): void {
+      this.fundService.getFundProjects().subscribe({
+        next: (funds: FundData[]) => {
+          this.fundList = funds;
+          this.error = null;  // clear any previous errors
+
+          // subscribe to paraMap changes to update the page view
+          this.route.paramMap.subscribe(params => {
+            const id = Number(params.get('id'));
+            if (id){
+              this.currentIndex = this.fundList.findIndex(funds => funds.fundId === id);
+              this.fund = this.fundList[this.currentIndex];
+            }
+          });
+        },
+        error: err => {
+          this.error = 'Error fetching funds list';
+          console.error('Error fetching funds list: ', err);
+        }
+      });
+  }
+
+  // function to go back to fund-list view
+  goBack(): void {
+    this.router.navigate(['/funds']);
+  }
+
+  // function to move forward through array with overflow protection
+  goForward(): void {
+    if (this.currentIndex < this.fundList.length -1) {
+      this.currentIndex++;
+      this.router.navigate(['/funds', this.fundList[this.currentIndex].fundId]);
+    }
+  }
+
+  // function to move backward through array with overflow protection
+  goBackward(): void {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      this.router.navigate(['/funds', this.fundList[this.currentIndex].fundId]);
+    }
+  }
+
+  // @Input() fundedProjects?: FundData;
 
 }
--- src/app/Component/fundProject-list/fundProject-list.ts
+++ src/app/Component/fundProject-list/fundProject-list.ts
@@ -12,23 +12,32 @@ import {FundService} from '../../Services/fund-service';
   ]
 })
 export class FundProjectList implements OnInit {
+  //Placeholder values for the table
+  displayedColumns: string[] = ['fundName','fundId', 'fundCode','fundManager', 'portfolioManager'];
    fundProjectList: FundData[] = [];
+   error: string | null = null;    // var to hold an error message
   constructor(private fundService: FundService) {
-
+    // constructor primarily used for dependency injection
   }
-
 
 
   ngOnInit():void {
     this.fundService.getFundProjects().subscribe({
-      next: ( data : FundData[]) => this.fundProjectList = data,
-      error: err => console.error("Error fetching fund data",err),
-      complete:() => console.log('Fund data fetch complete!')
-    })
-
+      next: ( data : FundData[]) => {
+        this.fundProjectList = data;
+        this.error = null;  // clear any previous errors
+      },
+      error: err => {
+        this.error = 'Error fetching fundProjectList';  // set an error message
+        console.error("Error fetching fundProjectList", err);
+      },
+      complete: () => console.log("Fund data fetch complete!")
+    });
   }
   selectedFundProject?: FundData;
   selectFundProject(selectedFundProject: FundData) {
     this.selectedFundProject = selectedFundProject;
   }
+
+
 }
